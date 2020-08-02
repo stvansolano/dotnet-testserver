cf62f1616ba9a994f4d2b67c658139451cabcd44
stvansolano <stvansolano@outlook.com>
2020-08-02 16:46:54 -0600

    Initial project structure

---
+++ TestServer/TestServer.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.IO;
+using System.Reflection;
+
+using YourApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests
+{
+    public class TestBase
+    {
+        public TestServer CreateServer()
+        {
+            var path = Assembly.GetAssembly(typeof(TestBase))
+              .Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("appsettings.json", optional: false)
+                    .AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+
+
+            var testServer = new TestServer(hostBuilder);
+
+            /*
+            testServer.Host
+                .MigrateDbContext<YourDbContext>((context, services) =>
+                {
+                    var env = services.GetService<IWebHostEnvironment>();
+                    var settings = services.GetService<IOptions<YourContext>>();
+                    var logger = services.GetService<ILogger<YourContext>>();
+
+                    new CatalogContextSeed()
+                    .SeedAsync(context, env, settings, logger)
+                    .Wait();
+                })
+                .MigrateDbContext<YourDbContext>((_, __) => { });
+            */
+
+            return testServer;
+        }
+
+        public static class Get
+        {
+            private const int PageIndex = 0;
+            private const int PageCount = 4;
+
+            public static string Items(bool paginated = false)
+            {
+                return paginated
+                    ? "api/v1/catalog/items" + Paginated(PageIndex, PageCount)
+                    : "api/v1/catalog/items";
+            }
+
+            public static string ItemById(int id)
+            {
+                return $"api/v1/catalog/items/{id}";
+            }
+
+            public static string ItemByName(string name, bool paginated = false)
+            {
+                return paginated
+                    ? $"api/v1/catalog/items/withname/{name}" + Paginated(PageIndex, PageCount)
+                    : $"api/v1/catalog/items/withname/{name}";
+            }
+
+            public static string Types = "api/v1/catalog/catalogtypes";
+
+            public static string Brands = "api/v1/catalog/catalogbrands";
+
+            public static string Filtered(int catalogTypeId, int catalogBrandId, bool paginated = false)
+            {
+                return paginated
+                    ? $"api/v1/catalog/items/type/{catalogTypeId}/brand/{catalogBrandId}" + Paginated(PageIndex, PageCount)
+                    : $"api/v1/catalog/items/type/{catalogTypeId}/brand/{catalogBrandId}";
+            }
+
+            private static string Paginated(int pageIndex, int pageCount)
+            {
+                return $"?pageIndex={pageIndex}&pageSize={pageCount}";
+            }
+        }
+    }
+}
