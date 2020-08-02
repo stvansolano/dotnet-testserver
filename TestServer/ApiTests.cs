75122a5cb5316c09fb5cd1cf2bc5a19a4880678f
stvansolano <stvansolano@outlook.com>
2020-08-02 16:56:30 -0600

    Added API test

---
+++ TestServer/ApiTests.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class ApiServerShould : TestBase
+    {
+        [Theory]
+        [InlineData("/api/hello")]
+        public async Task Get_HelloWorld(string url)
+        {
+            // Arrange
+            var client = base.CreateServer();
+
+            // Act
+            var response = await client.CreateClient()
+									   .GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("Hello World!", await response.Content.ReadAsStringAsync());
+        }
+    }
+}
